614a1e586545b267b8d3c3c8ec477872ea707eb4
Jozef Lacko <jlacko27@gmail.com>
2018-06-11 21:20:09 +0200

    documentation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
-var DOMAIN_NAME = "ENTE DOMAIN NAME";
+/*
+    jquery is required!
+    
+    you can use this:
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+*/
 
+/**
+ * Set domain where to send requests 
+ */
+var DOMAIN_NAME = "ENTER DOMAIN NAME";
+
+/**
+ * Function is basic authentification in Jira 
+ * 
+ * @param {string} user 
+ * @param {string} password 
+ * @param {function with 1 parameter which will be response from the server} callback 
+ */
 function login(user, password, callback) {
 		
     var datos = {			
@@ -28,7 +45,20 @@ function login(user, password, callback) {
     });
 }
 
-function sendRequestToJira(issuename, project, enviroment, reproduction, user, password, type) {
+
+/**
+ * Function will authentificate in Jira and create a new Jira issue.
+ * Jira issue will be assigned to the user who will be passed into the function * 
+ * 
+ * @param {string} issuename    - description of the issue
+ * @param {string} project      - must be an existing project!
+ * @param {string} enviroment 
+ * @param {string} reproduction 
+ * @param {string} user 
+ * @param {string} password 
+ * @param {string} type 
+ */
+function createJiraIssue(issuename, project, enviroment, reproduction, user, password, type) {
 	
     var datos = {
         "username": user,     // authentification
